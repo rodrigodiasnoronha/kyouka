0b357371ded4ee77e266d2500461a56777206b1c
rodrigodiasnoronha <rodrigodnoronha@gmail.com>
2022-04-22 12:09:57 -0300

    git add .

--- src/core/database/repositories/GuildWelcomeRepository.ts
+++
@@ -1,7 +0,0 @@
-import { AppDataSource } from '../data-source';
-
-import { GuildWelcomeEntity } from '../entities/GuildWelcomeEntity';
-
-export const GuildWelcomeRepository = AppDataSource.getRepository(GuildWelcomeEntity).extend({
-    // custom methods here...
-});
